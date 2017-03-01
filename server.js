7749ea32cb1c2ee86bd8a8ccae42c2fcd73fa2ab
Suni <Suni>
2017-02-28 19:58:48 -0600

    got basic express on my serverjs

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 console.log('Hello World!');
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(4000, function () {
+  console.log('Example app listening on port 4000!')
+})
